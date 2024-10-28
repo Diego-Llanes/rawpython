8a54bb20a8e8c0cccbd547cf1c873eeeb56b7bc5
Diego Llanes <snowythedog13@gmail.com>
2024-10-28 10:47:25 -0700

    Steal skeletonkeys setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,23 @@ from pathlib import Path
 
 readme = Path(__file__).parent / 'README.md'
 
-with open(readme) as f:
-    description = '\n'.join([line for line in f][2:])
+with open(readme, "r") as f:
+    long_description = f.read()
 
 setup(
     name='rawpython',
-    version='0.1',
+    version='{{VERSION_PLACEHOLDER}}',
     description="a library for people who hate libraries",
-    long_description=description,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Diego-Llanes/rawpython",
     author='Diego Llanes',
     author_email='rawpython@diegollanes.com',
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
 )
